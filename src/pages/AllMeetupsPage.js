f36cd05b24aabd122e6bd551da7cf8341113510c
Roberth González <reliutg@gmail.com>
2024-11-27 14:09:53 +0100

    refactor MeetupItem component to accept item as a prop and update AllMeetupsPage to fetch and display meetups dynamically

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,16 +1,22 @@
+import { useFetch } from "../util-hooks/useFetch";
+
 import MeetupItem from "../components/meetups/MeetupItem";
 import classes from "./../components/meetups/MeetupList.module.css";
 
-
 export default function AllMeetupsPage() {
+   const { data } = useFetch({
+    url: "/data.json",
+  });
+
+  if (!data) return <p>Loading...</p>;
+
   return (
     <section>
       <h1>All Meetups</h1>
       <ul className={classes.list}>
-        <MeetupItem />
-        <MeetupItem />
-        <MeetupItem />
-        <MeetupItem />
+        {data.map((item) => (
+          <MeetupItem key={item.id} item={item} />
+        ))}
       </ul>
     </section>
   );
