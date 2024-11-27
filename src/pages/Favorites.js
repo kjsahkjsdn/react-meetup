9fadb697dfb64dcbb796a130745d57a1fac49dad
Roberth González <reliutg@gmail.com>
2024-11-27 14:23:27 +0100

    add MeetupList component to FavoritesPage to display user's favorite meetups

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,15 @@
+import { useAppSelector } from "../state/favorites/hook";
+import { selectFavorites } from "../state/favorites/slice";
+
+import MeetupList from "../components/meetups/MeeupList";
+
 export default function FavoritesPage() {
+  const favorites = useAppSelector(selectFavorites);
+
   return (
     <section>
       <h1>Favorites Page</h1>
+       <MeetupList data={favorites} />
     </section>
   );
 }
