3d5fe21337ab85e6ab09d56b38902f0fe2793024
Roberth González <reliutg@gmail.com>
2024-11-28 09:57:53 +0100

    refactor RegisteredUser class to adhere to Single Responsibility Principle by separating service creation and multimedia content handling

--- ex1.ts
+++ ex1.ts
@@ -110,3 +110,15 @@ const services = [
 const user = new RegisteredUser(services);
 
 console.log(user.getTotal())
+
+/**
+ * Explicación:
+ * 
+ * Para identificar los problemas en el código, primero que hice fue aplicar los principios SOLID,
+ * y al hacerlo me di cuenta que el código no cumplía con el principio de responsabilidad única,
+ * ya que la clase RegisteredUser estaba haciendo más de una cosa, estaba calculando el total de los servicios
+ * por lo que decidí separar la creación de los servicios en una clase y manejar diferentes tipos de contenido multimedia en clases.
+ * 
+ * Ahora servicios se crean en una clase diferente y se manejan diferentes tipos de contenido multimedia en clases.
+ * Se usa inyección de dependencias para pasar los servicios a la clase RegisteredUser.
+ */
